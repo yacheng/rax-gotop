63b2857690a0b4a4eb6ec364cf4906a4f77f4cc5
yacheng.sz <yacheng.sz@taobao.com>
2019-01-17 16:00:38 +0800

    feat: test

---
+++ src/__tests__/index.js
@@ -0,0 +1,9 @@
+import {createElement, Component} from 'rax';
+import renderer from 'rax-test-renderer';
+import Gotop from '../';
+
+describe('Gotop', () => {
+  it('test typeof Gotop', () => {
+    expect(typeof Gotop).toEqual('function');
+  });
+});
